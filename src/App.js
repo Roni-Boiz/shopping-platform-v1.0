d36be2fcf4a43be3739bdb6cad80fc0ef8d643bc
Roni-Boiz <2019cs155@stu.ucsc.lk>
2022-09-01 18:41:15 +0530

    add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Products from "./components/Products";
+import Products from "./components/ProductList";
+import ProductView from "./components/ProductView";
+import ProductList from "./components/ProductList";
 
 function App() {
+
+  const [search, setSearch] = useState('');
+
+  function handleSearch(e) {
+    e.preventDefault();
+    console.log("Process the search")
+  }
   return (
     <>
-      <Navbar />
+      <Navbar 
+        term={search} 
+        handleChange={(value) => setSearch(value)} 
+        handleSearch = {e => handleSearch(e)} 
+      />
       <br />
-      <Products />
+      {/* Term is : {search} */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<ProductList/>} />
+          <Route exact path="/productView" element={<ProductView/>} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>  
   );
