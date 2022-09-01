c11f1621419a749852cc3c9a5e64c0dd210f7928
Roni-Boiz <2019cs155@stu.ucsc.lk>
2022-09-01 14:54:51 +0530

    add products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Products from "./components/Products";
 
 function App() {
   return (
     <>
       <Navbar />
+      <br />
+      <Products />
       <Footer />
     </>  
   );
