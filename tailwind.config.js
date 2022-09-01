955676a8f8b935ec4e364e076e466af504dd16f6
Roni-Boiz <2019cs155@stu.ucsc.lk>
2022-09-01 14:29:35 +0530

    Install and configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    "./src/**/*.{html,js,jsx}"
+  ],
+  content: [
+    "./src/**/*.{js, jsx, ts, tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
